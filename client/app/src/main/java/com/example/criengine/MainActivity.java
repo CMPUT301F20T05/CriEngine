dc211933ad7e7ea37e78a117bdc11b72c7fc0721
Robert Mah <rjmah1@ualberta.ca>
2020-10-21 19:44:00 -0600

    feat: added menu navigation for testing concurrent activities

--- client/app/src/main/java/com/example/criengine/MainActivity.java
+++ client/app/src/main/java/com/example/criengine/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.criengine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button profileActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // router to other activities for testing
+        profileActivityButton = findViewById(R.id.profile_activity_button);
+
+        profileActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ProfileActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 }
