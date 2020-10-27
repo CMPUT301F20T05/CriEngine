7795cafdf3e8a00334087487c44748f422e61c4b
AdityaHarvi <45128157+AdityaHarvi@users.noreply.github.com>
2020-10-27 14:24:13 -0600

    feat: add myBooks activity and its filer fragment

---
+++ client/app/src/main/java/com/example/criengine/MyBooksActivity.java
@@ -0,0 +1,14 @@
+package com.example.criengine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_books);
+    }
+}
---
+++ client/app/src/main/java/com/example/criengine/MyBooksAdapter.java
@@ -0,0 +1,4 @@
+package com.example.criengine;
+
+public class MyBooksAdapter {
+}
