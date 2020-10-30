02ad9642a30e173da39c4d620560ff557a135ff5
Azeez Abass <abassazeez6@gmail.com>
2020-10-29 19:16:09 -0600

    feat: clean up request action buttons, use single onClickHandler

--- client/app/src/main/java/com/example/criengine/Adapters/BorrowerBooksListAdapter.java
+++ client/app/src/main/java/com/example/criengine/Adapters/BorrowerBooksListAdapter.java
@@ -13,7 +13,11 @@ import androidx.annotation.Nullable;
 
 import com.example.criengine.Database.DatabaseWrapper;
 import com.example.criengine.Objects.Book;
+import com.example.criengine.Objects.Profile;
 import com.example.criengine.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
@@ -46,12 +50,12 @@ public class BorrowerBooksListAdapter extends ArrayAdapter<Book> {
 
         TextView bookNameTextView = view.findViewById(R.id.bookNameTextView);
         TextView bookStatusTextView = view.findViewById(R.id.bookStatusTextView);
-        Button bookActionButton = view.findViewById(R.id.bookActionButton);
+        final Button bookActionButton = view.findViewById(R.id.bookActionButton);
 
-        Book book = bookItems.get(position);
+        final Book book = bookItems.get(position);
+        final DatabaseWrapper dbw = DatabaseWrapper.getWrapper();
 
         bookNameTextView.setText(book.getTitle());
-
         bookStatusTextView.setText(book.getStatus());
 
         // TODO: use db username and watchlist to determine state
@@ -59,45 +63,37 @@ public class BorrowerBooksListAdapter extends ArrayAdapter<Book> {
         // Scan -- we have borrowed / Status == Accepted
         // Ok -- Rejected
         //if ( book.getRequesters().contains(Database.myUsername) || Database.getMyProfile.getWatchList().contains(book))
-        DatabaseWrapper dbw = DatabaseWrapper.getWrapper();
         if (book.getRequesters().contains(dbw.userId)) {
             bookActionButton.setText("Cancel");
-            bookActionButton.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            // TODO: remove request from profile && book
-                        }
-                    }
-            );
-
         } else if (book.getBorrower() == dbw.userId) {
             bookStatusTextView.setTextColor(view.getResources().getColor(R.color.status_accepted));
             bookActionButton.setText("Scan");
-            bookActionButton.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            // TODO : navigate to scan page
-                        }
-                    }
-            );
         } else {
             bookStatusTextView.setText("Rejected");
             bookStatusTextView.setTextColor(view.getResources().getColor(R.color.status_rejected));
             bookActionButton.setText("Ok");
-            bookActionButton.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            // TODO: remove request from profile
-                        }
-                    }
-            );
         }
 
+        bookActionButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        switch (bookActionButton.getText().toString()) {
+                            case("Scan"):
+                                // TODO: navigate to scan page
+                                break;
+                            case("Cancel"):
+                                // TODO: removes request from book
+                                // Do not break, execute Ok case as well
+                            case("Ok"):
+                                // TODO: removes request from profile
+                                // TODO: reload ListView
+                                break;
+                        }
+                    }
+                }
+        );
 
         return view;
     }
-
 }
