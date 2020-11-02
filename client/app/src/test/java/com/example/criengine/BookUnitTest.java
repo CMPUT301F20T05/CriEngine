a6500270acc3c753b36960c2bff79c5ade6f5ad0
AdityaHarvi <45128157+AdityaHarvi@users.noreply.github.com>
2020-11-01 18:05:34 -0700

    fix: reworded the class header

--- client/app/src/test/java/com/example/criengine/BookUnitTest.java
+++ client/app/src/test/java/com/example/criengine/BookUnitTest.java
@@ -1,18 +1,14 @@
 package com.example.criengine;
 
 import com.example.criengine.Objects.Book;
-
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Unit tests for the Book.java class.
+ * These tests test every method in the class.
  */
 public class BookUnitTest {
     @Test
