2a9739d427d0e0e406e85466d029a8755fcec2dd
AdityaHarvi <45128157+AdityaHarvi@users.noreply.github.com>
2020-11-01 20:28:46 -0700

    fix: refactored tests

--- client/app/src/test/java/com/example/criengine/BookUnitTest.java
+++ client/app/src/test/java/com/example/criengine/BookUnitTest.java
@@ -1,7 +1,11 @@
 package com.example.criengine;
 
 import com.example.criengine.Objects.Book;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
@@ -11,166 +15,144 @@ import static org.junit.Assert.*;
  * These tests test every method in the class.
  */
 public class BookUnitTest {
+    Book mockBook;
+    ArrayList<String> newRequesterList;
+
+    @Before
+    public void init() {
+        newRequesterList = new ArrayList<>();
+        mockBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
+    }
+
     @Test
     public void testBookConstructorOne() {
-        Book newBook = new Book();
+        Book mockBook = new Book();
 
-        assertEquals(newBook.getRequesters().size(), 0);
+        assertEquals(mockBook.getRequesters().size(), 0);
     }
 
     @Test
     public void testBookConstructorTwo() {
         List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
+        Book mockBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
 
-        assertEquals(newBook.getBookID(), "ID 1");
-        assertEquals(newBook.getOwner(), "New Owner");
-        assertEquals(newBook.getTitle(), "New Title");
-        assertEquals(newBook.getAuthor(), "New Author");
-        assertEquals(newBook.getDescription(), "New Description");
-        assertEquals(newBook.getIsbn(), "New ISBN");
-        assertEquals(newBook.getStatus(), "New Status");
-        assertEquals(newBook.getBorrower(), "New Borrower");
-        assertEquals(newBook.getRequesters().size(), 0);
-        assertEquals(newBook.getGeolocation(), "New Geolocation");
-        assertEquals(newBook.getImageURL(), "New Image");
+        assertEquals(mockBook.getBookID(), "ID 1");
+        assertEquals(mockBook.getOwner(), "New Owner");
+        assertEquals(mockBook.getTitle(), "New Title");
+        assertEquals(mockBook.getAuthor(), "New Author");
+        assertEquals(mockBook.getDescription(), "New Description");
+        assertEquals(mockBook.getIsbn(), "New ISBN");
+        assertEquals(mockBook.getStatus(), "New Status");
+        assertEquals(mockBook.getBorrower(), "New Borrower");
+        assertEquals(mockBook.getRequesters().size(), 0);
+        assertEquals(mockBook.getGeolocation(), "New Geolocation");
+        assertEquals(mockBook.getImageURL(), "New Image");
     }
 
     @Test
     public void testBookConstructorThree() {
         List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
+        Book mockBook = new Book("New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
 
-        assertNull(newBook.getBookID());
-        assertEquals(newBook.getOwner(), "New Owner");
-        assertEquals(newBook.getTitle(), "New Title");
-        assertEquals(newBook.getAuthor(), "New Author");
-        assertEquals(newBook.getDescription(), "New Description");
-        assertEquals(newBook.getIsbn(), "New ISBN");
-        assertEquals(newBook.getStatus(), "New Status");
-        assertEquals(newBook.getBorrower(), "New Borrower");
-        assertEquals(newBook.getRequesters().size(), 0);
-        assertEquals(newBook.getGeolocation(), "New Geolocation");
-        assertEquals(newBook.getImageURL(), "New Image");
+        assertNull(mockBook.getBookID());
+        assertEquals(mockBook.getOwner(), "New Owner");
+        assertEquals(mockBook.getTitle(), "New Title");
+        assertEquals(mockBook.getAuthor(), "New Author");
+        assertEquals(mockBook.getDescription(), "New Description");
+        assertEquals(mockBook.getIsbn(), "New ISBN");
+        assertEquals(mockBook.getStatus(), "New Status");
+        assertEquals(mockBook.getBorrower(), "New Borrower");
+        assertEquals(mockBook.getRequesters().size(), 0);
+        assertEquals(mockBook.getGeolocation(), "New Geolocation");
+        assertEquals(mockBook.getImageURL(), "New Image");
     }
 
     @Test
     public void testBookConstructorFour() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status");
+        Book mockBook = new Book("New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status");
 
-        assertNull(newBook.getBookID());
-        assertEquals(newBook.getOwner(), "New Owner");
-        assertEquals(newBook.getTitle(), "New Title");
-        assertEquals(newBook.getAuthor(), "New Author");
-        assertEquals(newBook.getDescription(), "New Description");
-        assertEquals(newBook.getIsbn(), "New ISBN");
-        assertEquals(newBook.getStatus(), "New Status");
-        assertNull(newBook.getBorrower());
-        assertEquals(newBook.getRequesters().size(), 0);
-        assertNull(newBook.getGeolocation());
-        assertNull(newBook.getImageURL());
+        assertNull(mockBook.getBookID());
+        assertEquals(mockBook.getOwner(), "New Owner");
+        assertEquals(mockBook.getTitle(), "New Title");
+        assertEquals(mockBook.getAuthor(), "New Author");
+        assertEquals(mockBook.getDescription(), "New Description");
+        assertEquals(mockBook.getIsbn(), "New ISBN");
+        assertEquals(mockBook.getStatus(), "New Status");
+        assertNull(mockBook.getBorrower());
+        assertEquals(mockBook.getRequesters().size(), 0);
+        assertNull(mockBook.getGeolocation());
+        assertNull(mockBook.getImageURL());
     }
 
     @Test
     public void testRequesterMethods() {
-        ArrayList<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
+        mockBook.addRequesters("A new Requester");
+        assertEquals(mockBook.getRequesters().size(), 1);
+        assertEquals(mockBook.getRequesters().get(0), "A new Requester");
 
-        newBook.addRequesters("A new Requester");
-        assertEquals(newBook.getRequesters().size(), 1);
-        assertEquals(newBook.getRequesters().get(0), "A new Requester");
-
-        newBook.removeRequesters("A new Requester");
-        assertEquals(newBook.getRequesters().size(), 0);
+        mockBook.removeRequesters("A new Requester");
+        assertEquals(mockBook.getRequesters().size(), 0);
 
         newRequesterList.add("Requester 1");
         newRequesterList.add("Requester 2");
-        newBook.setRequesters(newRequesterList);
-        assertEquals(newBook.getRequesters(), newRequesterList);
+        mockBook.setRequesters(newRequesterList);
+        assertEquals(mockBook.getRequesters(), newRequesterList);
     }
 
     @Test
     public void testBorrowerMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
+        mockBook.setBorrower("A new Borrower");
+        assertEquals(mockBook.getBorrower(), "A new Borrower");
 
-        newBook.setBorrower("A new Borrower");
-        assertEquals(newBook.getBorrower(), "A new Borrower");
-
-        newBook.removeBorrower();
-        assertNull(newBook.getBorrower());
+        mockBook.removeBorrower();
+        assertNull(mockBook.getBorrower());
     }
 
     @Test
     public void testOwnerMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setOwner("A new Owner");
-        assertEquals(newBook.getOwner(), "A new Owner");
+        mockBook.setOwner("A new Owner");
+        assertEquals(mockBook.getOwner(), "A new Owner");
     }
 
     @Test
     public void testTitleMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setTitle("A new Title");
-        assertEquals(newBook.getTitle(), "A new Title");
+        mockBook.setTitle("A new Title");
+        assertEquals(mockBook.getTitle(), "A new Title");
     }
 
     @Test
     public void testAuthorMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setAuthor("A new Author");
-        assertEquals(newBook.getAuthor(), "A new Author");
+        mockBook.setAuthor("A new Author");
+        assertEquals(mockBook.getAuthor(), "A new Author");
     }
 
     @Test
     public void testDescriptionMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setDescription("A new Description");
-        assertEquals(newBook.getDescription(), "A new Description");
+        mockBook.setDescription("A new Description");
+        assertEquals(mockBook.getDescription(), "A new Description");
     }
 
     @Test
     public void testISBNMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setIsbn("A new ISBN");
-        assertEquals(newBook.getIsbn(), "A new ISBN");
+        mockBook.setIsbn("A new ISBN");
+        assertEquals(mockBook.getIsbn(), "A new ISBN");
     }
 
     @Test
     public void testStatusMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setStatus("A new Status");
-        assertEquals(newBook.getStatus(), "A new Status");
+        mockBook.setStatus("A new Status");
+        assertEquals(mockBook.getStatus(), "A new Status");
     }
 
     @Test
     public void testLocationMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setGeolocation("A new Location");
-        assertEquals(newBook.getGeolocation(), "A new Location");
+        mockBook.setGeolocation("A new Location");
+        assertEquals(mockBook.getGeolocation(), "A new Location");
     }
 
     @Test
     public void testImageMethods() {
-        List<String> newRequesterList = new ArrayList<>();
-        Book newBook = new Book("ID 1", "New Owner", "New Title", "New Author", "New Description", "New ISBN", "New Status", "New Borrower", newRequesterList, "New Geolocation", "New Image");
-
-        newBook.setImageURL("A new Image");
-        assertEquals(newBook.getImageURL(), "A new Image");
+        mockBook.setImageURL("A new Image");
+        assertEquals(mockBook.getImageURL(), "A new Image");
     }
 }
