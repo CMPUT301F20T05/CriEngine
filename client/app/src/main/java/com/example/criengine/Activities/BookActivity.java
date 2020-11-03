925434a6f651e6cc72a4ed5840cfdd05778575ae
Cynthia <43241070+shadowciaw@users.noreply.github.com>
2020-11-03 09:41:52 -0700

    feat: added owner and borrower fields to book activity (#78)

--- client/app/src/main/java/com/example/criengine/Activities/BookActivity.java
+++ client/app/src/main/java/com/example/criengine/Activities/BookActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.criengine.R;
 
@@ -14,6 +15,9 @@ public abstract class BookActivity extends AppCompatActivity {
     EditText bookAuthor;
     EditText bookISBN;
     EditText bookStatus;
+    EditText bookOwner;
+    TextView bookBorrowerLabel;
+    EditText bookBorrower;
     ImageView bookImage;
 
     @Override
@@ -27,6 +31,9 @@ public abstract class BookActivity extends AppCompatActivity {
         bookStatus = findViewById(R.id.bookView_status);
         bookISBN = findViewById(R.id.bookView_ISBN);
         bookImage = findViewById(R.id.bookView_image);
+        bookOwner = findViewById(R.id.bookView_owner);
+        bookBorrowerLabel = findViewById(R.id.bookView_borrower_label);
+        bookBorrower = findViewById(R.id.bookView_borrower);
 
         bookTitle.setTag(bookTitle.getKeyListener());
         bookDetail.setTag(bookDetail.getKeyListener());
