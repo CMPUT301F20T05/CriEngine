cf3afb8ef2b4116fa2cbbbf6499244110f05e086
AdityaHarvi <45128157+AdityaHarvi@users.noreply.github.com>
2020-11-05 16:31:11 -0700

    feat: error screen

---
+++ client/app/src/main/java/com/example/criengine/Activities/ErrorActivity.java
@@ -0,0 +1,14 @@
+package com.example.criengine.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.criengine.R;
+import android.os.Bundle;
+
+public class ErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+    }
+}
