8989209a56b8d1a3618213d35c940efcfd0f6792
Robert Mah <rjmah1@ualberta.ca>
2020-11-05 15:55:53 -0700

    feat: profile now works as a fragment

---
+++ client/app/src/main/java/com/example/criengine/Fragments/ProfileFragment.java
@@ -0,0 +1,47 @@
+package com.example.criengine.Fragments;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.criengine.R;
+
+/**
+ * General ProfileActivity class that sets up common features.
+ */
+public abstract class ProfileFragment extends RootFragment {
+
+    ImageButton userImageButton;
+    TextView userTextView;
+    EditText bioEditText;
+    EditText phoneEditText;
+    EditText addressEditText;
+
+    @Override
+    public int initRootFrag() {
+        return R.layout.component_profile;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // Must setContentView(layoutID) in child before calling this super method.
+        super.onViewCreated(view, savedInstanceState);
+
+        // get all the UI components
+        userImageButton = view.findViewById(R.id.user_image);
+        userTextView = view.findViewById(R.id.user_profile_text);
+        bioEditText = view.findViewById(R.id.bio_text);
+        phoneEditText = view.findViewById(R.id.phone_text);
+        addressEditText = view.findViewById(R.id.addr_text);
+
+        // save the key listeners
+        bioEditText.setTag(bioEditText.getKeyListener());
+        phoneEditText.setTag(phoneEditText.getKeyListener());
+        addressEditText.setTag(addressEditText.getKeyListener());
+    }
+}
---
+++ client/app/src/main/java/com/example/criengine/Interfaces/IOnBackPressed.java
@@ -0,0 +1,10 @@
+package com.example.criengine.Interfaces;
+
+public interface IOnBackPressed {
+    /**
+     * If you return true the back press will not be taken into account,
+     * otherwise the activity will act naturally
+     * @return true if your processing has priority if not false
+     */
+    boolean onBackPressed();
+}
