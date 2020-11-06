c2d3c3206376bc48cae50db3fa4181fc07c97d5e
shadowciaw <shadowcharmeleon@gmail.com>
2020-11-06 11:17:10 -0700

    added javadocs

--- client/app/src/main/java/com/example/criengine/Fragments/ErrorFragment.java
+++ client/app/src/main/java/com/example/criengine/Fragments/ErrorFragment.java
@@ -13,9 +13,17 @@ import com.example.criengine.Fragments.ProfileFragment;
 import com.example.criengine.Interfaces.IOnBackPressed;
 import com.example.criengine.R;
 
+/**
+ * Error/placeholder Fragment.
+ * Temporary placeholder for search page while it is developed.
+ * To be used as error landing later.
+ */
 public class ErrorFragment extends RootFragment {
 
-
+    /**
+     * Returns the layout.
+     * @return The layout.
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
