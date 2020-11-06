b92000e746b2e1dd3e40bffe533f6924a06ad784
SamLojpur <samlojpur@gmail.com>
2020-11-06 14:49:15 -0700

    fix: change tests to use intentTest login (#136)

--- client/app/src/androidTest/java/com/example/criengine/ViewMyBookTest.java
+++ client/app/src/androidTest/java/com/example/criengine/ViewMyBookTest.java
@@ -32,19 +32,13 @@ public class ViewMyBookTest {
     @Before
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
-    }
 
-    /**
-     * Test to see if the editing the title and saving it will truly make the change.
-     */
-    @Test
-    public void editTitleTest() {
         // Asserts that the current activity is the LoginActivity.
         solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
 
-        // Get view for EditText and enter a city name
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextEmail), "user2@email.com");
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextPassword), "password");
+        // Input username and password
+        solo.enterText((EditText) solo.getView(R.id.loginEditTextEmail), "intentTestingUser@email.com");
+        solo.enterText((EditText) solo.getView(R.id.loginEditTextPassword), "intentTesting");
 
         solo.clickOnButton("Login");
 
@@ -55,6 +49,14 @@ public class ViewMyBookTest {
         // at least 1 match.
         assertTrue(solo.waitForText("My Books", 1, 10000));
 
+        addBook();
+    }
+
+    /**
+     * Test to see if the editing the title and saving it will truly make the change.
+     */
+    @Test
+    public void editTitleTest() {
         solo.clickInList(0);
 
         // Enter in a new title and save changes.
@@ -67,7 +69,9 @@ public class ViewMyBookTest {
 
         // Test to see if the changes were acknowledged.
         assertTrue(solo.waitForText("Delete Book", 1, 2000));
-        assertTrue(solo.waitForText("This is a new title.", 1, 2000));
+        assertTrue(solo.waitForText("Mock Title", 1, 2000));
+        solo.goBack();
+
     }
 
     /**
@@ -75,22 +79,6 @@ public class ViewMyBookTest {
      */
     @Test
     public void cancelEditTitleTest() {
-        // Asserts that the current activity is the LoginActivity.
-        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
-
-        // Get view for EditText and enter a city name
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextEmail), "user2@email.com");
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextPassword), "password");
-
-        solo.clickOnButton("Login");
-
-        // Asserts that the current activity is the RootActivity.
-        solo.assertCurrentActivity("Wrong Activity", RootActivity.class);
-
-        // Returns True if you can find "My Books" on the screen. Waits 10 seconds to find
-        // at least 1 match.
-        assertTrue(solo.waitForText("My Books", 1, 10000));
-
         solo.clickInList(0);
 
         // Enter in a new book title and cancel changes.
@@ -103,7 +91,9 @@ public class ViewMyBookTest {
 
         // Make sure that the changes were not saved.
         assertTrue(solo.waitForText("Delete Book", 1, 2000));
-        assertFalse(solo.waitForText("This is a new title.", 1, 2000));
+        assertTrue(solo.waitForText("This is a new book Title", 1, 2000));
+        solo.goBack();
+
     }
 
     /**
@@ -111,37 +101,20 @@ public class ViewMyBookTest {
      */
     @Test
     public void deleteBookTest() {
-        // Asserts that the current activity is the LoginActivity.
-        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
-
-        // Get view for EditText and enter a city name
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextEmail), "user2@email.com");
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextPassword), "password");
-
-        solo.clickOnButton("Login");
-
-        // Asserts that the current activity is the RootActivity.
-        solo.assertCurrentActivity("Wrong Activity", RootActivity.class);
-
-        // Returns True if you can find "My Books" on the screen. Waits 10 seconds to find
-        // at least 1 match.
-        assertTrue(solo.waitForText("My Books", 1, 10000));
-
         solo.clickInList(0);
-
-        String title = solo.getView(R.id.bookView_title).toString();
 
         // Click on delete book.
         assertTrue(solo.waitForText("Delete Book", 1, 2000));
         solo.clickOnButton("Delete Book");
 
         // Wait for the prompt.
-        assertTrue(solo.waitForText("Are you sure you want to delete this book?", 1, 2000));
-        solo.clickOnText("Delete");
+//        assertTrue(solo.waitForText("Are you sure you want to delete this book?", 1, 2000));
+        solo.clickOnText("DELETE");
 
         // Make sure we changed screens.
         solo.sleep(2000);
         assertTrue(solo.waitForText("My Books", 1, 2000));
+
     }
 
     /**
@@ -150,22 +123,6 @@ public class ViewMyBookTest {
      */
     @Test
     public void cancelDeleteBookTest() {
-        // Asserts that the current activity is the LoginActivity.
-        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
-
-        // Get view for EditText and enter a city name
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextEmail), "user2@email.com");
-        solo.enterText((EditText) solo.getView(R.id.loginEditTextPassword), "password");
-
-        solo.clickOnButton("Login");
-
-        // Asserts that the current activity is the RootActivity.
-        solo.assertCurrentActivity("Wrong Activity", RootActivity.class);
-
-        // Returns True if you can find "My Books" on the screen. Waits 10 seconds to find
-        // at least 1 match.
-        assertTrue(solo.waitForText("My Books", 1, 10000));
-
         solo.clickInList(0);
 
         assertTrue(solo.waitForText("Delete Book", 1, 2000));
@@ -177,6 +134,33 @@ public class ViewMyBookTest {
         // Make sure we didnt change screens.
         solo.sleep(2000);
         assertTrue(solo.waitForText("Delete Book", 1, 2000));
+        solo.goBack();
+
+    }
+
+    public void addBook() {
+        solo.clickOnButton("Add A Book");
+
+
+        assertTrue(solo.waitForText("Save", 1, 2000));
+
+        solo.enterText((EditText) solo.getView(R.id.newBookTitle), "Mock Title");
+        solo.enterText((EditText) solo.getView(R.id.newBookDesc), "Mock Description");
+        solo.enterText((EditText) solo.getView(R.id.newBookAuthor), "Mock Author");
+        solo.enterText((EditText) solo.getView(R.id.newBookISBN), "Mock ISBN");
+        solo.enterText((EditText) solo.getView(R.id.newBookImageURL), "Mock input");
+
+        solo.clickOnButton("Save");
+    }
+
+    public void deleteBook() {
+        if (solo.searchText("This is a new title.") || solo.searchText("Mock Title")) {
+            solo.clickInList(0);
+
+            solo.clickOnButton("Delete Book");
+            solo.clickOnText("DELETE");
+        }
+
     }
 
     /**
@@ -185,6 +169,7 @@ public class ViewMyBookTest {
      */
     @After
     public void tearDown() throws Exception{
+        deleteBook();
         solo.finishOpenedActivities();
     }
 }
