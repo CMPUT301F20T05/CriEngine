994a55e43f5d60cfe67097c5aabea4064fc7376c
Azeez Abass <abassazeez6@gmail.com>
2020-11-20 12:53:38 -0700

    fix: button order

--- client/app/src/main/java/com/example/criengine/Fragments/BottomNavFragment.java
+++ client/app/src/main/java/com/example/criengine/Fragments/BottomNavFragment.java
@@ -3,14 +3,16 @@ package com.example.criengine.Fragments;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.criengine.Activities.RootActivity;
 import com.example.criengine.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
